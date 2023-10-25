077cdb9cb6b293ac5d78b520d0b0fb8b9b65799d
jbock <jbock-java@gmx.de>
2023-10-25 19:58:34 +0200

    init

---
+++ haus_vom_nikolaus.py
@@ -0,0 +1,21 @@
+import math
+import turtle as t
+a = 300
+b = 144
+t.shape("turtle")
+t.begin_fill()
+t.color("red1", "purple2")
+t.left(180)
+t.forward(150)
+t.right(180)
+t.forward(a)
+t.left(b)
+t.forward(a)
+t.left(b)
+t.forward(a)
+t.left(b)
+t.forward(a)
+t.left(b)
+t.forward(a)
+t.mainloop()
+t.end_fill()
