49fcf802e3520f2b6585cbe1096ca72a84eae64d
jbock <jbock-java@gmx.de>
2023-12-10 11:54:50 +0100

    indien make_autopct

--- india.py
+++ india.py
@@ -2,24 +2,22 @@ import matplotlib.pyplot as plt
 import numpy
 
 labels = ['Waren', 'Dienstleistungen']
-sizes_indien = numpy.array([453400, 308676])
-sizes_deutschland=numpy.array([1657577, 406202])
 
-def absolute_value_indien(val):
-    a  = numpy.rint((val * sizes_indien.sum()) / 100)
-    return '{:.0f}'.format(a)
+def make_autopct(values):
+    def my_autopct(pct):
+        total = sum(values)
+        val = numpy.rint((pct * total) / 100)
+        return '{:.0f}'.format(val)
+    return my_autopct
     
-def absolute_value_deutschland(val):
-    a  = numpy.rint((val * sizes_deutschland.sum()) / 100)
-    return '{:.0f}'.format(a)
+def plot(ax, sizes):
+	ax.pie(sizes, labels=labels, autopct=make_autopct(sizes))
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 fig.set_size_inches(10, 6)
 ax1.legend(title="Indien 2022")
 ax2.legend(title="Deutschland 2022")
-ax1.pie(sizes_indien, labels=labels, autopct=absolute_value_indien)
-ax2.pie(sizes_deutschland, labels=labels, autopct=absolute_value_deutschland)
-plt.savefig('exporte_indien_deutschland_2022.png')
-#plt.show()
-
-
+plot(ax1, [453400, 308676])
+plot(ax2, [1657577, 406202])
+#plt.savefig('exporte_indien_deutschland_2022.png')
+plt.show()
