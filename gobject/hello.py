2c48c9d5bb6c254a9de3b3f7c19a4432fcdeb9e6
h908714124 <jbock-java@gmx.de>
2025-12-24 08:42:55 +0100

    escape code

--- gobject/hello.py
+++ gobject/hello.py
@@ -4,10 +4,18 @@ gi.require_version("Gtk", "4.0")
 from gi.repository import GLib, Gtk
 
 class MainWindow(Gtk.ApplicationWindow):
+    
+    def on_key_press(self, keyval, keycode, state, user_data):
+        if keycode == 65307:
+            self.close()
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         long_label = Gtk.Label(label="Here is a very long chunk of text.")
         self.set_child(long_label)
+        keycont = Gtk.EventControllerKey()
+        keycont.connect("key-pressed", self.on_key_press)
+        self.add_controller(keycont)
 
 class MyApplication(Gtk.Application):
     def __init__(self):
