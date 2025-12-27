cd56ed4d9e6fcf08d33b27e80b82595899f2dde1
jbock <jbock-java@gmx.de>
2025-12-27 13:04:26 +0100

    qml use pwscore

--- rettung/src/rettung_controller.py
+++ rettung/src/rettung_controller.py
@@ -10,6 +10,8 @@ from PySide6.QtCore import (
 from PySide6.QtQml import QmlElement
 from PySide6.QtWidgets import QApplication
 
+from tempfile import NamedTemporaryFile
+
 import threading
 import subprocess
 
@@ -27,6 +29,17 @@ def create_error(message):
 
 def create_success(message):
     return {"message": message, "type": "success"}
+
+def check_pw(string):
+    pwfile = NamedTemporaryFile().name
+    with open(pwfile, "w") as outfile:
+        outfile.write(string)
+    with open(pwfile, "r") as infile:
+        result = subprocess.run(["/usr/bin/pwscore"], stdin=infile, capture_output=True, text=True,
+            env={"LANG":"de_DE.UTF-8"})
+        if result.returncode != 0:
+            return result.stderr.rstrip()
+    return None
 
 @QmlElement
 class RettungController(QObject):
@@ -47,26 +60,44 @@ class RettungController(QObject):
             create_success("yum " * 3),
         ]
 
-    def set_password(self, pw):
-        subprocess.run(["sleep", "2"],
-                capture_output=True, text=True)
-        QApplication.restoreOverrideCursor()
-        self._state = "final"
-        self._messages.insert(0, create_success("OK " * 3))
+    def insert_message(self, message):
+        self._messages.insert(0, message)
         self.signal_messages_changed.emit()
+
+    def clear_messages(self):
+        self._messages.clear()
+        self.signal_messages_changed.emit()
+
+    def change_state(self, state):
+        if self._state == state:
+            return
+        self._state = state
         self.signal_state_changed.emit()
+
+    def set_password(self):
+        error = check_pw(self._pw)
+        if error:
+            self.change_state("unlocked")
+            self.insert_message(create_error(error))
+            return
+        subprocess.run(["sleep", "2"])
+        QApplication.restoreOverrideCursor()
+        self.change_state("final")
+        self.insert_message(create_success("OK " * 3))
 
     @Slot()
     def onSubmit(self):
         if self._state == "final":
             QCoreApplication.instance().quit()
             return
+        if len(self._pw) < 8 or self._pw != self._confirm:
+            self.change_state("locked")
+            self.insert_message(create_error("passphrase mismatch"))
+            return
         QApplication.setOverrideCursor(Qt.CursorShape.WaitCursor)
-        self._state = "busy"
-        self._messages.clear()
-        self.signal_messages_changed.emit()
-        self.signal_state_changed.emit()
-        threading.Thread(target = lambda : self.set_password("pw")).start()
+        self.change_state("busy")
+        self.clear_messages()
+        threading.Thread(target = lambda : self.set_password()).start()
 
     @Property(str, notify=signal_text_changed)
     def messageText(self):
@@ -100,12 +131,6 @@ class RettungController(QObject):
 
     def update_lockstate(self):
         if len(self._pw) >= 8 and self._pw == self._confirm:
-            if self._state == "unlocked":
-                return
-            self._state = "unlocked"
-            self.signal_state_changed.emit()
+            self.change_state("unlocked")
         else:
-            if self._state == "locked":
-                return
-            self._state = "locked"
-            self.signal_state_changed.emit()
+            self.change_state("locked")
